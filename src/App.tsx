b46b164b33bd5f80da99cd485a31ad7abfd93f73
Aynur Mindarov <aynurmotigulov@gmail.com>
2025-05-22 19:54:07 +0300

    fix: downloading

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
-import { useEffect, useRef, useState } from "react"
+/* eslint-disable sonarjs/no-nested-functions */
+import yorek from "@assets/kartinki/serdtse.svg"
+import animePhoto from "@assets/photo/anime.jpg"
+import aynurbekPhoto from "@assets/photo/Aynurbek2.png"
+import bolgarPhoto from "@assets/photo/bolgar.jpg"
+import ikeuPhoto from "@assets/photo/ikeu.jpg"
+import zilyousssPhoto from "@assets/photo/zilechka.jpg"
+import { ReactNode, useEffect, useRef, useState } from "react"
 import ContentPage from "./components/ContentPage/ContentPage"
 import EnvelopeInviteAnimation from "./components/EnvelopeInviteAnimation/EnvelopeInviteAnimation"
 import "./App.scss"
 
 function App() {
   const [animationCompleted, setAnimationCompleted] = useState(false)
+  const [resourcesLoaded, setResourcesLoaded] = useState(false)
   const [windowWidth, setWindowWidth] = useState(window.innerWidth)
   const audioRef = useRef<HTMLAudioElement | null>(null)
   const [isPlaying, setIsPlaying] = useState(false)
+
+  useEffect(() => {
+    const imageUrls = [aynurbekPhoto, zilyousssPhoto, ikeuPhoto, bolgarPhoto, animePhoto, yorek]
+
+    const imagePromises = imageUrls.map(
+      (url) =>
+        new Promise<void>((res, rej) => {
+          const img = new Image()
+          img.src = url
+          img.onload = () => res()
+          img.onerror = () => rej()
+        }),
+    )
+
+    Promise.all([...imagePromises])
+      .then(() => setResourcesLoaded(true))
+      .catch(() => {
+        setResourcesLoaded(true)
+      })
+  }, [])
 
   const togglePlay = () => {
     const audio = audioRef.current
@@ -21,7 +49,6 @@ function App() {
     setIsPlaying((prev) => !prev)
   }
 
-  // Обновляем ширину при изменении размера
   useEffect(() => {
     const handleResize = () => setWindowWidth(window.innerWidth)
     window.addEventListener("resize", handleResize)
@@ -29,29 +56,34 @@ function App() {
   }, [])
 
   const isSmallScreen = windowWidth < 640
+  let content: ReactNode
 
-  return (
-    <div className="app">
-      {!animationCompleted ? (
-        <EnvelopeInviteAnimation
-          isSmallScreen={isSmallScreen}
-          onOpenComplete={() => {
-            setAnimationCompleted(true)
-            togglePlay()
-          }}
-        />
-      ) : (
-        <ContentPage
-          isSmallScreen={isSmallScreen}
-          animationCompleted={animationCompleted}
-          togglePlay={togglePlay}
-          isPlaying={isPlaying}
-          setIsPlaying={setIsPlaying}
-          audioRef={audioRef}
-        />
-      )}
-    </div>
-  )
+  if (!animationCompleted) {
+    content = (
+      <EnvelopeInviteAnimation
+        isSmallScreen={isSmallScreen}
+        onOpenComplete={() => {
+          setAnimationCompleted(true)
+          togglePlay()
+        }}
+      />
+    )
+  } else if (!resourcesLoaded) {
+    content = <div className="loading">Загружается содержимое…</div>
+  } else {
+    content = (
+      <ContentPage
+        isSmallScreen={isSmallScreen}
+        animationCompleted={animationCompleted}
+        togglePlay={togglePlay}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        audioRef={audioRef}
+      />
+    )
+  }
+
+  return <div className="app">{content}</div>
 }
 
 export default App
